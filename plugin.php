3161f86c642b51754e0d6c5568ac83d6806f983b
Ángel Yarmas <angelyarmas.27@gmail.com>
2025-01-29 17:15:56 -0500

    add post install scripts

--- plugin.php
+++ plugin.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * Plugin Name:         Pedestal
+ * Plugin Name:         Pedestal Plugin Name
  * Plugin URI:          https://www.pedestalagency/plugins/
- * Description:         Pedestal boilerplate plugin.
+ * Description:         Pedestal Plugin description
  * Requires at least:   6.1
  * Requires PHP:        7.0
  * Version:             0.1.0
- * Author:              Pedestal
+ * Author:              Author Name
  * License:             GPL-2.0-or-later
  * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:         pedestal-before-after-gallery
