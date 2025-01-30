5443768462e7dd9e7b1592c1e08c78cb7ecce13a
Ángel Yarmas <angelyarmas.27@gmail.com>
2025-01-30 00:19:30 -0500

    chore: update version to 0.0.2 and simplify changelog for initial release

--- changelog.md
+++ changelog.md
@@ -1,7 +1,2 @@
 ## v0.0.1
-* Support for auto-registering custom post types.
-* Support for auto-registering Gutenberg blocks.
-* Easy config facade for plugin internal options.
-* Easy setup of actions and filters.
-* Easy registartion of custom REST routes.
-* Helper classes for internal file handling.
+* Initial release
--- my-plugin-boilerplate.php
+++ my-plugin-boilerplate.php
@@ -6,7 +6,7 @@
  * Description:         Pedestal Plugin description
  * Requires at least:   6.1
  * Requires PHP:        7.0
- * Version:             0.1.0
+ * Version:             0.0.2
  * Author:              Author Name
  * License:             GPL-2.0-or-later
  * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
